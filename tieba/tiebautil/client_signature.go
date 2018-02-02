8b99d82a7c048168c11d6d35cd060ab877b9bcf5
konica <2571583272@qq.com>
2018-02-02 18:24:22 +0800

    更新注释

--- tieba/tiebautil/client_signature.go
+++ tieba/tiebautil/client_signature.go
@@ -8,11 +8,12 @@ import (
 	"strings"
 )
 
-// TiebaClientSignature 根据给定贴吧客户端的 post (post数据指针) 进行签名, 以通过百度服务器验证。返回值为: sign 签名字符串
+// TiebaClientSignature 根据给定贴吧客户端的 post数据 进行签名, 以通过百度服务器验证。返回值为 sign 签名字符串值
 func TiebaClientSignature(post map[string]string) {
 	if post == nil {
 		return
 	}
+
 	// 预设
 	post["_client_type"] = "2"
 	post["_client_version"] = "6.9.2.1"
@@ -20,6 +21,7 @@ func TiebaClientSignature(post map[string]string) {
 	post["from"] = "mini_ad_wandoujia"
 	post["model"] = "HUAWEI NXT-AL10"
 	post["cuid"] = "61464018582906C485355A89D105ECFB|286245630389068"
+
 	var keys []string
 	for key := range post {
 		keys = append(keys, key)
@@ -34,7 +36,7 @@ func TiebaClientSignature(post map[string]string) {
 	post["sign"] = fmt.Sprintf("%x", md5.Sum(bb.Bytes()))
 }
 
-// TiebaClientRawQuerySignature 给 rawQuery 进行贴吧客户端签名
-func TiebaClientRawQuerySignature(rawQuery string) (sign string) {
+// TiebaClientRawQuerySignature 给 rawQuery 进行贴吧客户端签名, 返回值为签名后的 rawQuery
+func TiebaClientRawQuerySignature(rawQuery string) (signedRawQuery string) {
 	return rawQuery + "&sign=" + fmt.Sprintf("%x", md5.Sum([]byte(strings.Replace(rawQuery, "&", "", -1)+"tiebaclient!!!")))
 }
