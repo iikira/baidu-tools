0ff1a9d80a9a34dc5c744f989f6fe178303e64cb
iikira <2571583272@qq.com>
2021-01-16 16:02:24 +0800

    up

--- tieba/tieba_login.go
+++ tieba/tieba_login.go
@@ -3,9 +3,9 @@ package tieba
 import (
 	"fmt"
 	"github.com/bitly/go-simplejson"
-	"github.com/iikira/BaiduPCS-Go/requester"
 	"github.com/iikira/baidu-tools"
 	"github.com/iikira/baidu-tools/tieba/tiebautil"
+	"github.com/iikira/iikira-go-utils/requester"
 	"strconv"
 	"time"
 )
