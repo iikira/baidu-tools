7c0eda88869818b2a41eb323947de4d199f33ddc
skiy <skiychan@outlook.com>
2021-09-03 21:17:29 +0800

    bugfix: TiebaClientRawQuerySignature

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.15
 require (
 	github.com/bitly/go-simplejson v0.5.0
 	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
-	github.com/iikira/iikira-go-utils v0.0.0-20210116072744-75d8667ed431 // indirect
+	github.com/iikira/iikira-go-utils v0.0.0-20210116072744-75d8667ed431
 	github.com/kr/pretty v0.2.1 // indirect
 )
--- tieba/tiebautil/client_signature.go
+++ tieba/tiebautil/client_signature.go
@@ -1,14 +1,13 @@
 package tiebautil
 
 import (
-	"bytes"
 	"crypto/md5"
 	"encoding/hex"
-	"github.com/iikira/baidu-tools/randominfo"
 	"sort"
 	"strconv"
 	"strings"
-	"unsafe"
+
+	"github.com/iikira/baidu-tools/randominfo"
 )
 
 // TiebaClientSignature 根据给定贴吧客户端的post数据进行签名, 以通过百度服务器验证. 返回值为签名后的 post
@@ -56,7 +55,7 @@ func TiebaClientSignature(post map[string]string) {
 // TiebaClientRawQuerySignature 给 rawQuery 进行贴吧客户端签名, 返回值为签名后的 rawQuery
 func TiebaClientRawQuerySignature(rawQuery string) (signedRawQuery string) {
 	m := md5.New()
-	m.Write(bytes.Replace(*(*[]byte)(unsafe.Pointer(&rawQuery)), []byte("&"), nil, -1))
+	m.Write([]byte(strings.ReplaceAll(rawQuery, "&", "")))
 	m.Write([]byte("tiebaclient!!!"))
 
 	signedRawQuery = rawQuery + "&sign=" + strings.ToUpper(hex.EncodeToString(m.Sum(nil)))
